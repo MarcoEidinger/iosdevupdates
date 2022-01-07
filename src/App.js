538017c8aaef1f7517e1ac8605af927c92a40805
Marco Eidinger <marco.eidinger@sap.com>
2022-01-07 07:09:37 -0800

    site update

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#2196f3" class="w-6 h-6 mx-2">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
-                        <label>Site refresh occurs every 3h</label>
+                        <label>Site refresh occurs every 3h --- CHECK</label>
                     </div>
                 </div>
                 <header className="text-2xl p-4">
